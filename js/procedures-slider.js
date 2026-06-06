f5cb12a9d7bfc0561adfacf6fd22b93bf35094be
langiman2-beep <langiman2@gmail.com>
2026-06-06 21:22:30 +0300

    начал делать адаптацию

--- js/procedures-slider.js
+++ js/procedures-slider.js
@@ -1,10 +1,26 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+    clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.carousel-button--next',
+    prevEl: '.prev',
   },
 });
